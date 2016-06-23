98c8cadf31e6f0f49ead32291b4d34312e0abce6
freseco <freseco@gmail.com>
2016-06-23 15:23:47 +0200

    UI improvements

---
+++ Assets/Scripts/SpawnGameObjectsLand.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGameObjectsLand : MonoBehaviour {
+
+	public GameObject spawnPrefab;
+
+	public float minSecondsBetweenSpawning = 3.0f;
+	public float maxSecondsBetweenSpawning = 6.0f;
+	
+	public Transform chaseTarget;
+	
+	private float savedTime;
+	private float secondsBetweenSpawning;
+
+	// Use this for initialization
+	void Start () {
+		savedTime = Time.time;
+		secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// exit if there is a game manager and the game is over
+		if (GameManager.gm) {
+			if (GameManager.gm.gameIsOver)
+				return;
+		}
+
+		if (Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
+		{
+			MakeThingToSpawn();
+			savedTime = Time.time; // store for next spawn
+			secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+		}	
+	}
+
+	void MakeThingToSpawn()
+	{
+		// create a new gameObject
+		GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
+
+		// set chaseTarget if specified
+		if ((chaseTarget != null) && (clone.gameObject.GetComponent<Chaser> () != null))
+		{
+			clone.gameObject.GetComponent<Chaser>().SetTarget(chaseTarget);
+		}
+	}
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	private float speed=20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (speed * Time.deltaTime, speed * Time.deltaTime, speed * Time.deltaTime); 
+	}
+}
